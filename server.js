e04319dead00e9de98139519c87b716a67308c43
Kir2023 <kirill.kaydanovskyi@gmail.com>
2024-05-31 19:21:16 +0300

    Add endpoint to fetch all customers

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const PORT = process.env.PORT || 3000;
 
 const app = require("./app");
 
+const Customer = mongoose.model('Customer', customerSchema);
+
+// Эндпоинт для получения всех пользователей
+app.get('/api/customers', async (req, res) => {
+  try {
+    const customers = await Customer.find();
+    res.json(customers);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`);
 });
