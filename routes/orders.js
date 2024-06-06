b5ccca47aa2569994e7f48e28154511ce3f1bd5b
Kir2023 <kirill.kaydanovskyi@gmail.com>
2024-06-06 16:45:39 +0300

    add filters

--- routes/orders.js
+++ routes/orders.js
@@ -4,7 +4,20 @@ const Order = require("../models/order");
 
 router.get("/", async (req, res, next) => {
   try {
-    const orders = await Order.find().exec();
+    const { sortBy, filterBy, filterValue } = req.query;
+
+    let query = {};
+    if (filterBy && filterValue) {
+      query[filterBy] = filterValue;
+    }
+
+    let sort = {};
+    if (sortBy) {
+      const [field, order] = sortBy.split(":");
+      sort[field] = order === "desc" ? -1 : 1;
+    }
+
+    const orders = await Order.find(query).sort(sort).exec();
     res.json(orders);
   } catch (error) {
     next(error);
